16983ecd8f77a5286d0eafb5e69d265a85d91c56
Search <96184824+Saerch@users.noreply.github.com>
2022-04-28 17:28:17 +0530

    First commit

---
+++ src/Component/Multi.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Multi(){
+    return(
+        <div>
+            <h1 data-ns-test="project-name">Dream Project</h1>
+             <p data-ns-test="project-description">Project Description</p>
+        </div>
+    )
+}
+
+export default Multi;
